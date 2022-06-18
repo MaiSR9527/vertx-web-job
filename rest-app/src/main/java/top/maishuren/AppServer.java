c83839fbad2012d5962b6779c8e45eddf7fbcd21
maishuren <maisrcn@qq.com>
2022-06-18 14:01:42 +0800

    vertx web demo

---
+++ rest-app/src/main/java/top/maishuren/AppServer.java
@@ -0,0 +1,98 @@
+package top.maishuren;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.maishuren.util.Runner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author MaiShuRen
+ * @site https://www.maishuren.top
+ * @since 2022-06-16 23:57
+ **/
+public class AppServer extends AbstractVerticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(AppServer.class);
+
+    private Map<String, JsonObject> products = new HashMap<>();
+
+    @Override
+    public void start() throws Exception {
+
+        setUpInitialData();
+
+        Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        router.get("/products/:productID").handler(this::handleGetProduct);
+        router.put("/products/:productID").handler(this::handleAddProduct);
+        router.get("/products").handler(this::handleListProducts);
+        logger.info("server is starting on vertx framework...");
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
+    }
+
+    public static void main(String[] args) {
+        Runner.runExample(AppServer.class);
+    }
+
+    private void handleGetProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = products.get(productID);
+            if (product == null) {
+                sendError(404, response);
+            } else {
+                response.putHeader("content-type", "application/json").end(product.encodePrettily());
+            }
+        }
+    }
+
+    private void handleAddProduct(RoutingContext routingContext) {
+        String productID = routingContext.request().getParam("productID");
+        HttpServerResponse response = routingContext.response();
+        if (productID == null) {
+            sendError(400, response);
+        } else {
+            JsonObject product = routingContext.getBodyAsJson();
+            if (product == null) {
+                sendError(400, response);
+            } else {
+                products.put(productID, product);
+                response.end();
+            }
+        }
+    }
+
+    private void handleListProducts(RoutingContext routingContext) {
+        JsonArray arr = new JsonArray();
+        products.forEach((k, v) -> arr.add(v));
+        routingContext.response().putHeader("content-type", "application/json").end(arr.encodePrettily());
+    }
+
+    private void sendError(int statusCode, HttpServerResponse response) {
+        response.setStatusCode(statusCode).end();
+    }
+
+    private void setUpInitialData() {
+        addProduct(new JsonObject().put("id", "prod3568").put("name", "Egg Whisk").put("price", 3.99).put("weight", 150));
+        addProduct(new JsonObject().put("id", "prod7340").put("name", "Tea Cosy").put("price", 5.99).put("weight", 100));
+        addProduct(new JsonObject().put("id", "prod8643").put("name", "Spatula").put("price", 1.00).put("weight", 80));
+    }
+
+    private void addProduct(JsonObject product) {
+        products.put(product.getString("id"), product);
+    }
+}
